973e91d88a774663be25b50b2118d21502da83d4
Igor <igorbod.webdev@gmail.com>
2020-03-09 20:53:22 +0700

    create header and made main slider with slick slider

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 $(function() {
+  // main slider
   $('.header__slider').slick({
     infinite: true,
-    fade: true
+    fade: true,
+    prevArrow: '<img src="img/icons/arrow-left.svg" alt="Previous" class="slick-prev header-slide-prev">',
+    nextArrow: '<img src="img/icons/arrow-right.svg" alt="Next" class="slick-next header-slide-next">'
   });
 });
 
