762618022852b3b5b708c01496be040aaee782d7
Igor <igorbod.webdev@gmail.com>
2020-03-09 23:00:36 +0700

    made nav slide for main slider using slick slider syncing

--- js/main.js
+++ js/main.js
@@ -4,7 +4,17 @@ $(function() {
     infinite: true,
     fade: true,
     prevArrow: '<img src="img/icons/arrow-left.svg" alt="Previous" class="slick-prev header-slide-prev">',
-    nextArrow: '<img src="img/icons/arrow-right.svg" alt="Next" class="slick-next header-slide-next">'
+    nextArrow: '<img src="img/icons/arrow-right.svg" alt="Next" class="slick-next header-slide-next">',
+    asNavFor: '.slider-dots'
   });
-});
+  
+  // nav for Main slider
+  $('.slider-dots').slick({
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false,
+    asNavFor: '.header__slider'
+  });
 
+});
