63301c7f53880c1a812352402714bd2333452b83
Igor <igorbod.webdev@gmail.com>
2020-03-14 16:01:20 +0700

    add counter(guests, nights) for section 'sleep' slider

--- js/main.js
+++ js/main.js
@@ -44,6 +44,40 @@ window.addEventListener('DOMContentLoaded', function () {
     nextArrow: '<img src="img/icons/arrow-right.svg" alt="Next" class="slick-next slider-next slider-next_holder slider-btn-holder">'
   });
 
+  /* COUNTERS ==========================================================================*/
+  $('<div class="quantity-nav"><div class="quantity-button quantity-down">-</div></div><div class="quantity-button quantity-up">+</div>').insertAfter('.quantity input');
+  $('.quantity').each(function () {
+    var spinner = $(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find('.quantity-up'),
+      btnDown = spinner.find('.quantity-down'),
+      min = input.attr('min'),
+      max = input.attr('max');
+
+    btnUp.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue >= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+    btnDown.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+  });
+  /* =================================================================================== */
+
   // change header map parts
   let buttonSlider = document.querySelectorAll('.slider-btn-header'),
       sliderIndex = 0;
